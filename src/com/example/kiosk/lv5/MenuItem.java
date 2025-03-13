51ab3a384e0a5dd3bdeb526e332ca08269e58c26
gganb <gksql0801@naver.com>
2025-03-13 13:46:30 +0900

    Feat : 캡슐화

--- src/com/example/kiosk/lv5/MenuItem.java
+++ src/com/example/kiosk/lv5/MenuItem.java
@@ -1,11 +1,11 @@
-package com.example.kiosk.lv4;
+package com.example.kiosk.lv5;
 
 //메뉴 세부 속성
 public class MenuItem {
 
-    String itemName;    // 이름
-    double itemPrice;   // 가격
-    String itemInfo;   // 메뉴 설명
+    private final String itemName;    // 이름
+    private final double itemPrice;   // 가격
+    private final String itemInfo;    // 메뉴 설명
 
     public MenuItem(String itemName, double itemPrice, String itemInfo) {
         this.itemName = itemName;
@@ -13,4 +13,16 @@ public class MenuItem {
         this.itemInfo = itemInfo;
     }
 
+    // getter
+    public String getItemName() {
+        return itemName;
+    }
+
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public String getItemInfo() {
+        return itemInfo;
+    }
 }
