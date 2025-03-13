f974055ab25aff5950129951101c2f4b2e3f031c
gganb <gksql0801@naver.com>
2025-03-13 17:49:34 +0900

    Feat : 장바구니 클래스 장바구니 리스트 출력, 장바구니에 메뉴항목 추가, 장바구니 초기화 기능 추가

--- src/com/example/kiosk/challenge/Cart.java
+++ src/com/example/kiosk/challenge/Cart.java
@@ -1,4 +1,38 @@
 package com.example.kiosk.challenge;
 
+import java.util.List;
+
 public class Cart {
+    private final List<MenuItem> cartList;
+    private int cnt;     // 수량
+
+    public Cart(List<MenuItem> cartList) {
+        this.cartList = cartList;
+    }
+
+    public List<MenuItem> getCartList() {
+        return cartList;
+    }
+
+    public void printCartList() {
+        for (int i = 0; i < cartList.size(); i++) {
+            MenuItem cartItem = cartList.get(i);
+            System.out.printf("%-10s | W %-2s | %s\n", cartItem.getItemName(), cartItem.getItemPrice(), cartItem.getItemInfo());
+        }
+    }
+
+    /**
+     * 장바구니에 추가
+     *
+     * @param menuChoice
+     */
+    public void addCartList(Menu menu, int menuChoice) {
+        MenuItem selectItem = menu.getMenuItem(menuChoice); //menu == null
+        cartList.add(selectItem);
+        System.out.println(selectItem.getItemName() + " 가(이) 장바구니에 추가되었습니다.");
+        cnt++;
+    }
+    public void clearCartList(){
+        cartList.clear();
+    }
 }
