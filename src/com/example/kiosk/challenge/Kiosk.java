5203eb3494f73e35f506335e8c947415c9f80bc6
gganb <gksql0801@naver.com>
2025-03-13 17:48:07 +0900

    Feat : 장바구니 추가 장바구니와 연결완료

--- src/com/example/kiosk/challenge/Kiosk.java
+++ src/com/example/kiosk/challenge/Kiosk.java
@@ -1,4 +1,4 @@
-package com.example.kiosk.lv5;
+package com.example.kiosk.challenge;
 
 import java.util.List;
 import java.util.Scanner;
@@ -8,10 +8,13 @@ public class Kiosk {
 
     private final List<Menu> menuList;
     private Scanner scan;
+    private Cart cart; //..
 
-    public Kiosk(List<Menu> menuList) {
+    public Kiosk(List<Menu> menuList,List<MenuItem> cartList) {
         this.menuList = menuList;
         this.scan = new Scanner(System.in); // 필요할때
+        this.cart = new Cart(cartList);
+
     }
 
     // 키오스크 시작 함수
@@ -30,11 +33,27 @@ public class Kiosk {
                 if (choice == 0)
                     break;
 
-                String str = choiceNumber(choice, menuList);
-                if ("BACK".equals(str)) {
+                String back = choiceNumber(choice, menuList);
+                if ("BACK".equals(back)) {
+                    System.out.println();
                     continue;
-                } else if ("OK".equals(str)) {
-                    continue;
+                }
+                if(!cart.getCartList().isEmpty()){
+                    System.out.println("[ ORDER MENU ]");
+                    System.out.println("4. Orders       | 장바구니를 확인 후 주문합니다.");
+                    System.out.println("5. Cancel       | 진행중인 주문을 취소합니다.");
+
+                    int selectOrder = Integer.parseInt(scan.nextLine());
+                    if(selectOrder == 4){
+
+                        System.out.println("아래와 같이 주문하시겠습니까?");
+                        System.out.println("[ Orders ]");
+                        cart.printCartList();
+                    }else if(selectOrder ==5){
+                        System.out.println("주문을 취소합니다.");
+                        cart.clearCartList();
+
+                    }
                 }
 
             } catch (ArrayIndexOutOfBoundsException | IllegalCallerException e) {    // 지정된 범위 외의 번호 입력시 예외 처리
@@ -54,11 +73,12 @@ public class Kiosk {
         }
 
         Menu selectMenu = menuList.get(choice - 1);
-        System.out.println("[ " + selectMenu.getCategory().toUpperCase() + " MENU ]");
+        // 메뉴 아이템 리스트 출력
+        System.out.println();
         selectMenu.printMenu();
 
         // 리스트가 비어있을 경우
-        if (selectMenu.getMenuItems().isEmpty()) {
+        if (selectMenu.getMenuItemList().isEmpty()) {
             throw new IllegalCallerException("[ERROR] 존재하지 않는 항목 입니다.");
         }
         // 메뉴 선택 번호 입력
@@ -69,12 +89,26 @@ public class Kiosk {
             return "BACK";
         }
         // 범위 외 입력
-        if (menuChoice < 1 || menuChoice > selectMenu.getMenuItems().size()) {
+        if (menuChoice < 1 || menuChoice > selectMenu.getMenuItemList().size()) {
             throw new ArrayIndexOutOfBoundsException("[ERROR] 존재하지 않는 번호입니다!");
         }
 
-        // 메뉴 상세 정보 출력
+        // 메뉴 상세 정보 출력 menuChoice 가 입력한 번호 . ...
         selectMenu.printInfo(menuChoice - 1);
+
+        System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+        System.out.printf("%d. %s      %d. %s\n",1,"확인",2,"취소");
+        int makeCart = Integer.parseInt(scan.nextLine());
+        switch (makeCart){
+            case 1 :
+                cart.addCartList(selectMenu,menuChoice-1);
+                break;
+            case 2 :
+                return "BACK";
+            default :
+                System.out.println("다시 입력해주세요.");
+        }
+
         return "OK";
     }
 
@@ -86,6 +120,8 @@ public class Kiosk {
         }
         System.out.println("0. 종료      | 종료");
     }
+
+
 }
 
 
