1936bb8072d3e122f378b7fcf1ed9a04171cff94
gganb <gksql0801@naver.com>
2025-03-13 17:50:00 +0900

    :

--- src/com/example/kiosk/challenge/KioskMain.java
+++ src/com/example/kiosk/challenge/KioskMain.java
@@ -1,18 +1,17 @@
-package com.example.kiosk.lv5;
+package com.example.kiosk.challenge;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.example.kiosk.lv5.DataBuild.dataBuild;
-
+import static com.example.kiosk.challenge.DataBuild.dataBuild;
 
 // 시작 지점
 public class KioskMain {
     public static void main(String[] args) {
-
+        List<MenuItem> cartList = new ArrayList<>();
+        Cart cart = new Cart(cartList); //무관
         //키오스크에 메뉴 리스트를 넘김
-        Kiosk kiosk = new Kiosk(dataBuild());
-
+        Kiosk kiosk = new Kiosk(dataBuild(),cartList);
         // 키오스크 시작하기
         kiosk.start();
     }
