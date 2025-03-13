ae4e6080f3f88dc579ce61d49a855d1e369eae49
gganb <gksql0801@naver.com>
2025-03-13 13:43:40 +0900

    Fix : 메서드 수정 choiceNumber의 리턴값을 String에서 booleand으로 수정함 주석 수정

--- src/com/example/kiosk/lv5/Kiosk.java
+++ src/com/example/kiosk/lv5/Kiosk.java
@@ -1,23 +1,22 @@
-package com.example.kiosk.lv4;
+package com.example.kiosk.lv5;
 
 import java.util.List;
 import java.util.Scanner;
 
 // 프로그램 순서 및 흐름 제어 담당
 public class Kiosk {
-    // 메뉴 리스트를 담은 리스트 ㅋㅋ
-    private final List<Menu> menuList;  // 메뉴 카테고리 리스트
 
-    //위의 리스트로 키오스크를 초기화함 그래야지 각 카테고리에 맞는 음식을 엥 근데 키오스크 하나에
-    // 여러 카테고리가 있어야 하는거 아닌가? 아~ 메뉴 리스트는 메뉴 여러개 ㅇㅋ
+    private final List<Menu> menuList;
+    private Scanner scan;
+
     public Kiosk(List<Menu> menuList) {
         this.menuList = menuList;
+        this.scan = new Scanner(System.in); // 필요할때
     }
 
     // 키오스크 시작 함수
     public void start() {
-        // 스캐너 선언
-        Scanner scan = new Scanner(System.in);
+
         // 반복문 시작
         while (true) {
             try {
@@ -25,56 +24,79 @@ public class Kiosk {
                 printMainMenu();
 
                 // 메인 메뉴 선택 번호 입력받기
-                int choice = scan.nextInt();
-                if (choice == 0)
+                int choice = Integer.parseInt(scan.nextLine());
+
+                // 메인 메뉴 종료
+                if (choice == 0) {
+                    System.out.println();
+                    System.out.println("프로그램을 종료합니다.");
                     break;
-                // 메뉴 항목과 메뉴 리스트 연결함
-                choiceNumber(choice, menuList);
+                }
 
+                boolean back = choiceNumber(choice, menuList);
+                if (!back) {
+                    System.out.println();
+                    continue;
+                }
 
-            } catch (ArrayIndexOutOfBoundsException e) {    // 지정된 범위 외의 번호 입력시 예외 처리
+            } catch (ArrayIndexOutOfBoundsException | IllegalCallerException e) {    // 지정된 범위 외의 번호 입력시 예외 처리
                 System.out.println(e.getMessage());
+                System.out.println();
+            } catch (IllegalArgumentException i) {
+                System.out.println("[ERROR] 숫자를 입력해주세요.");
+                System.out.println();
             }
         }
     }
 
-    // 메인 메뉴 출력 -선택된 숫자에 따른 로직 이거는 출력 됨
-    public void choiceNumber(int choice, List<Menu> menuList) {
-        Scanner scan = new Scanner(System.in);  // 스캐너 다시
-       // if(choice ==0){    //입력된 숫자가 0이면 반환
-        //    return;
-        //}
-       if(choice < 1 || choice > menuList.size()){    // 메뉴 리스트 범위 밖의 숫자면 예외처리
-           throw new ArrayIndexOutOfBoundsException("존재하지 않는 번호입니다!");
-       }
-       Menu selectMenu = menuList.get(choice-1);   // 인덱스 번호로 입력되어야함.  selectMenu : 메뉴 카테고리 하나. == 메뉴 리스트 하나
-        System.out.println("[ " + selectMenu.getCategory().toUpperCase() + " MENU ]");   // 표지 출력
+    /**
+     * 번호 선택에 따른 로직 메뉴 아이템 리스트 >
+     *
+     * @param choice
+     * @param menuList
+     */
+    public boolean choiceNumber(int choice, List<Menu> menuList) {
+
+        // 메뉴 리스트 범위 밖의 숫자면 예외처리
+        if (choice < 1 || choice > menuList.size()) {
+            throw new ArrayIndexOutOfBoundsException("[ERROR] 존재하지 않는 번호입니다!");
+        }
+
+        Menu selectMenu = menuList.get(choice - 1);
+        // 메뉴 아이템 리스트 출력
+        System.out.println();
         selectMenu.printMenu();
 
-        if(selectMenu.getMenuItems().isEmpty()){    // 리스트가 비어있을 경우
-            System.out.println("선택한 메뉴에 항목이 없습니다.");
-            return;
+        // 리스트가 비어있을 경우
+        if (selectMenu.getMenuItems().isEmpty()) {
+            throw new IllegalCallerException("[ERROR] 존재하지 않는 항목 입니다.");
+        }
+        // 메뉴 선택 번호 입력
+        int menuChoice = Integer.parseInt(scan.nextLine());
+
+        // BACK 뒤로가기
+        if (menuChoice == 0) {
+            return false;
+        }
+        // 범위 외 입력
+        if (menuChoice < 1 || menuChoice > selectMenu.getMenuItems().size()) {
+            throw new ArrayIndexOutOfBoundsException("[ERROR] 존재하지 않는 번호입니다!");
         }
 
-        int menuChoice = scan.nextInt();    // 메뉴 선택하는 번호 입력받기
-        if(menuChoice == 0){    // 번호가 0 이면 . .. .  start() 호출
-            start();
-        }
-        if(menuChoice < 1 || menuChoice > selectMenu.getMenuItems().size()){ // 메인메뉴 > 메뉴 선택 > 뒤로가기 > 메인메뉴 > 0 입력하면 왜 여기로 한번 더 넘어가지?
-            throw new ArrayIndexOutOfBoundsException("존재하지 않는 번호입니다!");
-        }
-        selectMenu.printInfo(menuChoice-1); // 입력값 -1 = 인덱스 번호. 입력해서 해당 리스트 내의 아이템 정보 출력하기
-
-
-        }
-        public void printMainMenu(){
-            System.out.println("[ ✨MAIN MENU  ]");
-            for (int i = 0; i < menuList.size(); i++) {
-                System.out.println((i + 1) + ". " + menuList.get(i).getCategory()); // 카테고리 출력.
-            }
-            System.out.println("0. 종료      | 종료");
-        }
+        // 메뉴 상세 정보 출력
+        selectMenu.printInfo(menuChoice - 1);
+        return true;
     }
+
+    // 메뉴 카테고리 출력
+    public void printMainMenu() {
+        System.out.println("[ ✨MAIN MENU  ]");
+        for (int i = 0; i < menuList.size(); i++) {
+            System.out.println((i + 1) + ". " + menuList.get(i).getCategory());
+        }
+        System.out.println("0. 종료      | 종료");
+    }
+}
 
 
 
