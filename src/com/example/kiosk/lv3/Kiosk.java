9aa94f641481a47dcd118195c706b8d1f8df6e3c
gganb <gksql0801@naver.com>
2025-03-13 11:26:25 +0900

    Fix : scanner를 필드에서 선언만 하고 Kiosk 객체가 생성될 때마다 스캐너 객체를 생성하도록 변경

---
+++ src/com/example/kiosk/lv3/Kiosk.java
@@ -0,0 +1,72 @@
+package com.example.kiosk.lv3;
+
+
+import java.util.List;
+import java.util.Scanner;
+
+// 프로그램 순서 및 흐름 제어 담당
+public class Kiosk {
+    private final List<MenuItem> menuItemArray;
+
+    private Scanner scan;
+
+    public Kiosk(List<MenuItem> menuItemList) {
+        this.menuItemArray = menuItemList;
+        scan = new Scanner(System.in);
+    }
+
+    public void start() {
+        while (true) {
+            try {
+                // 번호 입력받기
+                int choice = Integer.parseInt(scan.nextLine());
+
+                choiceNumber(choice, menuItemArray);
+                if (choice == 0)
+                    break;
+            } catch (ArrayIndexOutOfBoundsException e) {    // 지정된 범위 외의 번호 입력시 예외 처리
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    // 선택된 숫자에 따른 로직
+    public void choiceNumber(int num, List<MenuItem> menuItemList) {
+        switch (num) {
+            case 1:
+                printInfo(menuItemList.get(0));
+                break;
+            case 2:
+                printInfo(menuItemList.get(1));
+                break;
+            case 3:
+                printInfo(menuItemList.get(2));
+                break;
+            case 4:
+                printInfo(menuItemList.get(3));
+                break;
+            case 0:
+                System.out.println("종료합니다.");
+                break;
+            default:
+                throw new ArrayIndexOutOfBoundsException("존재하지 않는 번호입니다.");
+        }
+    }
+
+    // 선택된 메뉴 출력
+    public void printInfo(MenuItem menuItem) {
+        System.out.println("이름 : " + menuItem.burgerName);
+        System.out.println("가격 : " + menuItem.burgerPrice);
+        System.out.println("설명 : " + menuItem.burgerInfo);
+    }
+
+    // 반복문으로 List 안에 있는 menuItemList 하니씩 출력
+    public void printMenu() {
+        System.out.println("[ SHAKESHACK MENU ]");
+        for (int i = 0; i < menuItemArray.size(); i++) {
+            System.out.printf("%s. %-15s | %-5sW | %s\n", (i + 1), menuItemArray.get(i).burgerName, menuItemArray.get(i).burgerPrice, menuItemArray.get(i).burgerInfo);
+        }
+        System.out.println("0. 종료하기          | ");
+    }
+
+}
