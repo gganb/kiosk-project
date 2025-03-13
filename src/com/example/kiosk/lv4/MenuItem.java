d1d8087304637477ca27a4a8510ee73347e07b16
gganb <gksql0801@naver.com>
2025-03-13 13:33:24 +0900

    Fix : 필드명 수정 메뉴 카테고리가 추가되었기에 필드명을 상위개념으로  추상화해 수정함

--- src/com/example/kiosk/lv4/MenuItem.java
+++ src/com/example/kiosk/lv4/MenuItem.java
@@ -1,16 +1,17 @@
-package com.example.kiosk.lv3;
+package com.example.kiosk.lv4;
 
 //메뉴 세부 속성
 public class MenuItem {
 
-    String burgerName;    // 이름
-    double burgerPrice;   // 가격
-    String burgerInfo;   // 메뉴 설명
+     String itemName;    // 이름
+     double itemPrice;   // 가격
+     String itemInfo;   // 메뉴 설명
 
-    public MenuItem(String burgerName, double burgerPrice, String burgerInfo) {
-        this.burgerName = burgerName;
-        this.burgerPrice = burgerPrice;
-        this.burgerInfo = burgerInfo;
+    public MenuItem(String itemName, double itemPrice, String itemInfo) {
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemInfo = itemInfo;
     }
+
 
 }
