32aeca03ec6653e3ec2168ce7094b3dcaeb59241
gganb <gksql0801@naver.com>
2025-03-13 13:24:55 +0900

    Fix : 주석 , 메뉴 출력 메서드 수정 메뉴 리스트의 제목을 Kiosk 클래스에서 따로 출력했던 코드를 Menu클래스의 메뉴 리스트 출력 메서드에 넣음

--- src/com/example/kiosk/lv4/Menu.java
+++ src/com/example/kiosk/lv4/Menu.java
@@ -1,21 +1,55 @@
-package com.example.kiosk.lv3;
+package com.example.kiosk.lv4;
 
-import java.util.ArrayList;
 import java.util.List;
 
 // menuItem 클래스 관리
 public class Menu {
-    List<Menu> menuItemList = new ArrayList<>();
-    MenuItem menuItem;
-    //
-    public Menu(String smokeShack, double v, String s) {
+    List<MenuItem> menuItemList;    // 메뉴 아이템 리스트
+    String category;                // 카테고리 이름 필드
+
+    // 생성자. 카테고리 이름과 아이템 리스트로 초기화
+    public Menu(String category, List<MenuItem> menuItemList) {
+        this.menuItemList = menuItemList;
+        this.category = category;
     }
 
-    public void initBurgerMenu(){
-        menuItemList.add(new Menu("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItemList.add(new Menu("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItemList.add(new Menu("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItemList.add(new Menu("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+    // getter
+    public List<MenuItem> getMenuItems() {
+        return menuItemList;
+    }
 
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * 메뉴 아이템 추가
+     * @param menuItem
+     */
+    public void addMenuItem(MenuItem menuItem) {
+        menuItemList.add(menuItem);
+    }
+
+    /**
+     * 선택한 메뉴 출력
+     *
+     * @param menuChoice
+     */
+    public void printInfo(int menuChoice) {
+        System.out.println();
+        MenuItem selectItem = menuItemList.get(menuChoice);
+        System.out.print("선택한 메뉴: ");
+        System.out.printf("%-10s | W %-2s | %s\n", selectItem.itemName, selectItem.itemPrice, selectItem.itemInfo);
+        System.out.println();
+    }
+
+    //메뉴 리스트 출력
+    public void printMenu() {
+        System.out.println("[ " + getCategory().toUpperCase() + " MENU ]");
+        for (int i = 0; i < menuItemList.size(); i++) {
+            MenuItem item = menuItemList.get(i);
+            System.out.printf("%s. %-15s | %-5sW | %s\n", (i + 1), item.itemName, item.itemPrice, item.itemInfo);
+        }
+        System.out.println("0. 뒤로가기          | ");
     }
 }
