b227b51131fa93c7b1c04602cbb921664696fec0
gganb <gksql0801@naver.com>
2025-03-14 11:18:32 +0900

    Fix : 주석제거

--- src/com/example/kiosk/challenge/KioskMain.java
+++ src/com/example/kiosk/challenge/KioskMain.java
@@ -1,8 +1,7 @@
 package com.example.kiosk.challenge;
 
-import java.util.ArrayList;
+
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static com.example.kiosk.challenge.DataBuild.dataBuild;
@@ -10,10 +9,12 @@ import static com.example.kiosk.challenge.DataBuild.dataBuild;
 // 시작 지점
 public class KioskMain {
     public static void main(String[] args) {
-        Map<MenuItem,Integer> cartList= new HashMap<>();
+
+        Map<MenuItem, Integer> cartList = new HashMap<>();
+
         Cart cart = new Cart(cartList);
-        //키오스크에 메뉴 리스트를 넘김
-        Kiosk kiosk = new Kiosk(dataBuild(),cart);
+
+        Kiosk kiosk = new Kiosk(dataBuild(), cart);
         // 키오스크 시작하기
         kiosk.start();
     }
