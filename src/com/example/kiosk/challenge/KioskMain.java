7e23836c9e0492b1982bea6a026b9058d13c5192
gganb <gksql0801@naver.com>
2025-03-14 10:10:37 +0900

    Fix : 선언문 수정 Cart클래스의 데이터타입 변경에 따라 선언문을 수정함

--- src/com/example/kiosk/challenge/KioskMain.java
+++ src/com/example/kiosk/challenge/KioskMain.java
@@ -1,17 +1,19 @@
 package com.example.kiosk.challenge;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.example.kiosk.challenge.DataBuild.dataBuild;
 
 // 시작 지점
 public class KioskMain {
     public static void main(String[] args) {
-        List<MenuItem> cartList = new ArrayList<>();
-        Cart cart = new Cart(cartList); //무관
+        Map<MenuItem,Integer> cartList= new HashMap<>();
+        Cart cart = new Cart(cartList);
         //키오스크에 메뉴 리스트를 넘김
-        Kiosk kiosk = new Kiosk(dataBuild(),cartList);
+        Kiosk kiosk = new Kiosk(dataBuild(),cart);
         // 키오스크 시작하기
         kiosk.start();
     }
