30b1cc9e085b9457404404ab2b6cb156470438c5
gganb <gksql0801@naver.com>
2025-03-14 11:58:00 +0900

    Fix : 잘못된 로직 변경 중복된 입력 해결, 마지막 주문에서 메뉴판 선택시 메뉴판으로 돌아가는 부분은 아직 해결하지못함

--- src/com/example/kiosk/challenge/CartManage.java
+++ src/com/example/kiosk/challenge/CartManage.java
@@ -9,46 +9,63 @@ public class CartManage {
     }
 
 
-    public void selectOrderMenu() {
-        int selectOrder = Input.getInputNum();
-        if (selectOrder == 4) {
-            System.out.println("아래와 같이 주문하시겠습니까?");
-            System.out.println("[ Orders ]");
-            cart.printCartList();
-            totalPrice();
-        } else if (selectOrder == 5) {
-            System.out.println("주문을 취소합니다.");
-            cart.clearCartList();
-
+    public void selectOrderMenu(int selectOrder) {
+        switch (selectOrder) {
+            case 4:
+                System.out.println("아래와 같이 주문하시겠습니까?");
+                System.out.println("[ Orders ]");
+                cart.printCartList();
+                totalPrice();
+                pay();
+                return;
+            case 5:
+                System.out.println("주문을 취소합니다.");
+                cart.clearCartList();
+                break;
         }
     }
 
     public boolean cartIsEmpty() {
-        if (cart.getCartList().isEmpty())
-            return true;
-        return false;
+        return cart.getCartList().isEmpty();
     }
 
-    public void addCartListManege(Menu menu, int menuChoice) {
+    public void addCartListManage(Menu menu, int menuChoice) {
         System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
         System.out.printf("%d. %s      %d. %s\n", 1, "확인", 2, "취소");
         int makeCart = Input.getInputNum();
         switch (makeCart) {
             case 1:
                 cart.addCartList(menu, menuChoice);
-                break;
+                return;
             case 2:
-                break;
+                return;
             default:
                 System.out.println("[ERROR] 존재하지 않는 번호입니다!");
         }
     }
 
     public void totalPrice() {
+        System.out.println();
         System.out.println("[ Total ]");
         System.out.println("W " + cart.getTotalPrice());
     }
 
+    public void pay() {
+        System.out.printf("%d. %s      %d. %s\n", 1, "주문", 2, "메뉴판");
+        int payNum = Input.getInputNum();
+        switch (payNum) {
+            case 1:
+                System.out.printf("주문이 완료되었습니다. 금액은 W %.1f 입니다.\n", cart.getTotalPrice());
+                cart.clearCartList();
+                return;
+            case 2:
+                return;
+            default:
+                System.out.println("[ERROR] 존재하지 않는 번호입니다!");
+
+        }
+
+    }
 
 }
 
