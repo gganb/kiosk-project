ac7362900d2c6e3d13e9441799a3dbac20028fcb
gganb <gksql0801@naver.com>
2025-03-14 10:09:05 +0900

    Feat : 장바구니 관리 클래스 키오스크에서 장바구니를 관리하는 것 보다 분리하는게 적절하다고 생각해 장바구니와 관련된 흐름을 담음

--- src/com/example/kiosk/challenge/CartManage.java
+++ src/com/example/kiosk/challenge/CartManage.java
@@ -1,4 +1,55 @@
 package com.example.kiosk.challenge;
 
+
 public class CartManage {
+    private final Cart cart;
+
+    public CartManage(Cart cart) {
+        this.cart = cart;
+    }
+
+
+    public void selectOrderMenu() {
+        int selectOrder = Input.getInputNum();
+        if (selectOrder == 4) {
+            System.out.println("아래와 같이 주문하시겠습니까?");
+            System.out.println("[ Orders ]");
+            cart.printCartList();
+            totalPrice();
+        } else if (selectOrder == 5) {
+            System.out.println("주문을 취소합니다.");
+            cart.clearCartList();
+
+        }
+    }
+
+    public boolean cartIsEmpty() {
+        if (cart.getCartList().isEmpty())
+            return true;
+        return false;
+    }
+
+    public void addCartListManege(Menu menu, int menuChoice) {
+        System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+        System.out.printf("%d. %s      %d. %s\n", 1, "확인", 2, "취소");
+        int makeCart = Input.getInputNum();
+        switch (makeCart) {
+            case 1:
+                cart.addCartList(menu, menuChoice);
+                break;
+            case 2:
+                break;
+            default:
+                System.out.println("[ERROR] 존재하지 않는 번호입니다!");
+        }
+    }
+
+    public void totalPrice() {
+        System.out.println("[ Total ]");
+        System.out.println("W " + cart.getTotalPrice());
+    }
+
+
 }
+
+
